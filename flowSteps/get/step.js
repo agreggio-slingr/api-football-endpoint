637d0e7fd5a19b6b3cb30f7f2ea7e601e6ee69c1
Alejandro Greggio <agreggio@slingr.io>
2022-12-01 16:01:23 -0300

    First Commit

--- flowSteps/get/step.js
+++ flowSteps/get/step.js
@@ -4,8 +4,13 @@
  * @param {text} bound, This is used to get a random number between 0 (inclusive) and the number passed in this argument, exclusive.
  */
 step.get = function (url) {
-
     var data = endpoint.get(url) ;
+
+    sys.logs.debug('[Test] GET from: ' + data.toJSON());
+
+    sys.logs.debug('[Test] GET from: ' + data);
+
+    sys.logs.debug('[Test] GET from: ' + data['text']);
 
     return {
         "response": data['text']
