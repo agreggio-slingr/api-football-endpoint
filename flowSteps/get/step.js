db8c916eaa596f64383a92c656a08da245f2f63b
Alejandro Greggio <agreggio@slingr.io>
2022-12-01 15:53:02 -0300

    First Commit

--- flowSteps/get/step.js
+++ flowSteps/get/step.js
@@ -7,18 +7,6 @@ step.get = function (url) {
 
     var data = endpoint.get(url) ;
 
-    sys.logs.info('[apifootball] GET from: ' + url);
-
-    sys.logs.info('[apifootball] GET from: ' + data);
-
-    sys.logs.info('[apifootball] GET from: ' + data.toJson());
-
-    sys.logs.info('[apifootball] GET from: ' + data['text']);
-
-    sys.logs.info('[apifootball] GET from: ' + data.field('text').val());
-
-    sys.logs.info('[apifootball] GET from: ' + data.field('text').val());
-
     return {
         "response": data['text']
     };
