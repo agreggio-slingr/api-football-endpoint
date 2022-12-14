9b950f2faeb817fe30cc7096068f2644e07d1b13
Alejandro Greggio <agreggio@slingr.io>
2022-12-14 15:02:03 -0300

    First Commit

--- flowSteps/generic/step.js
+++ flowSteps/generic/step.js
@@ -13,29 +13,31 @@ step.generic = function (method, baseUrl, body, callbackData, callbacks) {
 
 	let options = checkHttpOptions(baseUrl, body, headers);
 
-	switch (method) {
-		case 'get':
-			return endpoint._get(options, callbackData, callbacks);
-		case 'post':
-			return endpoint._post(options, callbackData, callbacks);
-		case 'delete':
-			return endpoint._delete(options, callbackData, callbacks);
-		case 'put':
-			return endpoint._put(options, callbackData, callbacks);
-		case 'connect':
-			return endpoint._connect(options, callbackData, callbacks);
-		case 'head':
-			return endpoint._head(options, callbackData, callbacks);
-		case 'options':
-			return endpoint._options(options, callbackData, callbacks);
-		case 'patch':
-			return endpoint._patch(options, callbackData, callbacks);
-		case 'trace':
-			return endpoint._trace(options, callbackData, callbacks);
-		default:
-			sys.logs.error('Invalid method received.');
-			break;
-	}
+	return "Respuesta";
+
+	// switch (method) {
+	// 	case 'get':
+	// 		return endpoint._get(options, callbackData, callbacks);
+	// 	case 'post':
+	// 		return endpoint._post(options, callbackData, callbacks);
+	// 	case 'delete':
+	// 		return endpoint._delete(options, callbackData, callbacks);
+	// 	case 'put':
+	// 		return endpoint._put(options, callbackData, callbacks);
+	// 	case 'connect':
+	// 		return endpoint._connect(options, callbackData, callbacks);
+	// 	case 'head':
+	// 		return endpoint._head(options, callbackData, callbacks);
+	// 	case 'options':
+	// 		return endpoint._options(options, callbackData, callbacks);
+	// 	case 'patch':
+	// 		return endpoint._patch(options, callbackData, callbacks);
+	// 	case 'trace':
+	// 		return endpoint._trace(options, callbackData, callbacks);
+	// 	default:
+	// 		sys.logs.error('Invalid method received.');
+	// 		break;
+	// }
 
 }
 
