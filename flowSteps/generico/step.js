30932294420e32cb9fddc91b4a4fec881e942505
Alejandro Greggio <agreggio@slingr.io>
2022-12-15 18:47:04 -0300

    First Commit

---
+++ flowSteps/generico/step.js
@@ -0,0 +1,16 @@
+/**
+ * This flow step will send generic request.
+ *
+ * @param {text} method, This is used to config method.
+ * @param {text} path, This is used to config external URL.
+ * @param {text} headers, This is used to config external URL.
+ * @param {text} params, This is used to config external URL.
+ * @param {text} body, This is used to send body request.
+ * @param {text} callbackData, This is used to send callback data.
+ * @param {text} callbacks, This is used to send callbacks.
+ */
+step.generico = function (method, path,headers,params, body, callbackData, callbacks) {
+
+	return "Mensaje";
+
+};
