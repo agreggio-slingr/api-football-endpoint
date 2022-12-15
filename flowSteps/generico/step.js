c39f2cbdb9512bf7a4008f41f25257dfcf442999
Alejandro Greggio <agreggio@slingr.io>
2022-12-15 18:50:41 -0300

    First Commit

--- flowSteps/generico/step.js
+++ flowSteps/generico/step.js
@@ -11,6 +11,41 @@
  */
 step.generico = function (method, path,headers,params, body, callbackData, callbacks) {
 
-	return "Mensaje";
+	sys.logs.error('Entro en la funcion.');
+
+    let options = {
+        path: path,
+        body: body,
+        headers:headers,
+        params:params
+    };
+
+	sys.logs.error('Resultado Option: ' + options);
+
+    return ('Resultado Option: ' + options);
+
+    // switch (method) {
+    // 	case 'get':
+    // 		return endpoint._get(options, callbackData, callbacks);
+    // 	case 'post':
+    // 		return endpoint._post(options, callbackData, callbacks);
+    // 	case 'delete':
+    // 		return endpoint._delete(options, callbackData, callbacks);
+    // 	case 'put':
+    // 		return endpoint._put(options, callbackData, callbacks);
+    // 	case 'connect':
+    // 		return endpoint._connect(options, callbackData, callbacks);
+    // 	case 'head':
+    // 		return endpoint._head(options, callbackData, callbacks);
+    // 	case 'options':
+    // 		return endpoint._options(options, callbackData, callbacks);
+    // 	case 'patch':
+    // 		return endpoint._patch(options, callbackData, callbacks);
+    // 	case 'trace':
+    // 		return endpoint._trace(options, callbackData, callbacks);
+    // 	default:
+    // 		sys.logs.error('Invalid method received.');
+    //         return null;
+    // }
 
 };
