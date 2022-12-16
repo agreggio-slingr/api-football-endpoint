7a25845aab02f692027e9ed0d160ffd8aa587a05
Alejandro Greggio <agreggio@slingr.io>
2022-12-16 16:48:54 -0300

    First Commit

--- flowSteps/generic/step.js
+++ flowSteps/generic/step.js
@@ -11,34 +11,57 @@
  */
 step.generic = function (method, path, headers,params, body, callbackData, callbacks) {
 
-    // body = {
-    //     path: path,
-    //     headers:headers,
-    //     params:params,
-	// 	body:body
-    // };
+	var options = checkHttpOption(path, body);
 
     switch (method) {
     	case 'get':
-    		return endpoint._get(body, callbackData, callbacks);
+    		return endpoint._get(options, callbackData, callbacks);
     	case 'post':
-    		return endpoint.post(path, body, callbackData, callbacks);
+    		return endpoint._post(options, callbackData, callbacks);
     	case 'delete':
-    		return endpoint._delete(body, callbackData, callbacks);
+    		return endpoint._delete(options, callbackData, callbacks);
     	case 'put':
-    		return endpoint._put(body, callbackData, callbacks);
+    		return endpoint._put(options, callbackData, callbacks);
     	case 'connect':
-    		return endpoint._connect(body, callbackData, callbacks);
+    		return endpoint._connect(options, callbackData, callbacks);
     	case 'head':
-    		return endpoint._head(body, callbackData, callbacks);
+    		return endpoint._head(options, callbackData, callbacks);
     	case 'options':
-    		return endpoint._options(body, callbackData, callbacks);
+    		return endpoint._options(options, callbackData, callbacks);
     	case 'patch':
-    		return endpoint._patch(body, callbackData, callbacks);
+    		return endpoint._patch(options, callbackData, callbacks);
     	case 'trace':
-    		return endpoint._trace(body, callbackData, callbacks);
+    		return endpoint._trace(options, callbackData, callbacks);
     	default:
             return null;
     }
 
 };
+
+var checkHttpOption = function (url, options) {
+	options = options || {};
+	if (!!url) {
+		if (isObject(url)) {
+			// take the 'url' parameter as the options
+			options = url || {};
+		} else {
+			if (!!options.path || !!options.params || !!options.body) {
+				// options contains the http package format
+				options.path = url;
+			} else {
+				// create html package
+				options = {
+					path: url,
+					body: options
+				}
+			}
+		}
+	}
+	return options;
+};
+
+var isObject = function (obj) {
+	return !!obj && stringType(obj) === '[object Object]'
+};
+
+var stringType = Function.prototype.call.bind(Object.prototype.toString);
