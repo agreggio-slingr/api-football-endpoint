0650d227470cece378afe72d34403fcafd2e7b43
Alejandro Greggio <agreggio@slingr.io>
2022-12-21 09:48:07 -0300

    First Commit

--- flowSteps/httpCall/step.js
+++ flowSteps/httpCall/step.js
@@ -12,7 +12,7 @@
 step.httpCall = function (method, path, headers, params, body, callbackData, callbacks) {
 
 	sys.logs.debug('[apifootball] path from: ' + body);
-	sys.logs.debug('[apifootball] path from: ' + JSON.parse(body));
+	sys.logs.debug('[apifootball] path from: ' + JSON.stringify(body));
 
 	body = body ? JSON.parse(body) : null
 	headers = headers ? JSON.parse(headers) : null
