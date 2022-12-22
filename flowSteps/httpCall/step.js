d8ed4821a1946e440be05ccb9608c415733e6cee
Alejandro Greggio <agreggio@slingr.io>
2022-12-22 11:32:07 -0300

    First Commit

--- flowSteps/httpCall/step.js
+++ flowSteps/httpCall/step.js
@@ -2,48 +2,57 @@
  * This flow step will send generic request.
  *
  * @param {text} method, This is used to config method.
- * @param {text} path, This is used to config external URL.
+ * @param {text} url, This is used to config external URL.
  * @param {string} headers, This is used to config external URL.
  * @param {string} params, This is used to config external URL.
  * @param {string} body, This is used to send body request.
  * @param {text} callbackData, This is used to send callback data.
  * @param {text} callbacks, This is used to send callbacks.
+ * @param {boolean} followRedirects, This is used to config follow redirects.
+ * @param {boolean} download, This is used to config download.
+ * @param {boolean} fullResponse, This is used to config full response.
  */
-step.httpCall = function (method, path, headers, params, body, callbackData, callbacks) {
+step.httpCall = function (method, url, headers, params, body, requiresCallBack,
+						  callbackData, callbacks,settings ,followRedirects, download, fullResponse) {
 
 	headers = isObject(headers) ? headers : stringToObject(headers)
 	params = isObject(params) ? params : stringToObject(params)
 	body = isObject(body) ? body : JSON.parse(body);
 
 	var options = {
-		path: path,
+		url: url,
 		params:params,
 		headers:headers,
-		body: body
+		body: body,
+		settings : {
+			followRedirects : followRedirects,
+			download : download,
+			fullResponse : fullResponse
+		}
 	}
 
-    switch (method) {
-    	case 'get':
-    		return endpoint._get(options, callbackData, callbacks);
-    	case 'post':
-    		return endpoint._post(options, callbackData, callbacks);
-    	case 'delete':
-    		return endpoint._delete(options, callbackData, callbacks);
-    	case 'put':
-    		return endpoint._put(options, callbackData, callbacks);
-    	case 'connect':
-    		return endpoint._connect(options, callbackData, callbacks);
-    	case 'head':
-    		return endpoint._head(options, callbackData, callbacks);
-    	case 'options':
-    		return endpoint._options(options, callbackData, callbacks);
-    	case 'patch':
-    		return endpoint._patch(options, callbackData, callbacks);
-    	case 'trace':
-    		return endpoint._trace(options, callbackData, callbacks);
-    	default:
-            return null;
-    }
+	switch (method) {
+		case 'get':
+			return endpoint._get(options, callbackData, callbacks);
+		case 'post':
+			return endpoint._post(options, callbackData, callbacks);
+		case 'delete':
+			return endpoint._delete(options, callbackData, callbacks);
+		case 'put':
+			return endpoint._put(options, callbackData, callbacks);
+		case 'connect':
+			return endpoint._connect(options, callbackData, callbacks);
+		case 'head':
+			return endpoint._head(options, callbackData, callbacks);
+		case 'options':
+			return endpoint._options(options, callbackData, callbacks);
+		case 'patch':
+			return endpoint._patch(options, callbackData, callbacks);
+		case 'trace':
+			return endpoint._trace(options, callbackData, callbacks);
+		default:
+			return null;
+	}
 
 };
 
