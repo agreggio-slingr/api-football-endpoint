780d1bd78ea1d8477614bb2bcab0805586593567
Alejandro Greggio <agreggio@slingr.io>
2023-01-10 13:54:56 -0300

    First Commit

--- flowSteps/httpCall/step.js
+++ flowSteps/httpCall/step.js
@@ -75,20 +75,20 @@ step.httpCall = function (stepConfig) {
 
 var parse = function (url, pathVariables){
 
-	// var regex = /{([^}]*)}/g;
-	//
-	// if (!url.match(regex)){
-	// 	return url;
-	// }
-	//
-	// if(!pathVariables){
-	// 	sys.logs.error('No path variables have been received and the url contains curly brackets\'{}\'');
-	// 	throw new Error('Error please contact support.');
-	// }
-	//
-	// url = url.replace(regex, function(m, i) {
-	// 	return pathVariables[i] ? pathVariables[i] : m;
-	// })
+	var regex = /{([^}]*)}/g;
+
+	if (!url.match(regex)){
+		return url;
+	}
+
+	if(!pathVariables){
+		sys.logs.error('No path variables have been received and the url contains curly brackets\'{}\'');
+		throw new Error('Error please contact support.');
+	}
+
+	url = url.replace(regex, function(m, i) {
+		return pathVariables[i] ? pathVariables[i] : m;
+	})
 
 	return url;
 }
